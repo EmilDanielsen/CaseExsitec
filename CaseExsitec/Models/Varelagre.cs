46d3d44ec603d4d815841bbe746b722ca605d059
Emil <Emil@DESKTOP-DVC7HD8>
2022-06-04 13:34:14 +0200

    Varelagre model

---
+++ CaseExsitec/Models/Varelagre.cs
@@ -0,0 +1,15 @@
+﻿namespace CaseExsitec.Models
+{
+    public class Varelagre
+    {
+        public int Id { get; set; }
+        public int Lagenr { get; set; }
+        public string? Sted { get; set; }
+
+        public Varelagre()
+        {
+
+        }
+
+    }
+}
